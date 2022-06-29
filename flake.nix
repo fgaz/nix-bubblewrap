4e078c85244963202b5cbd2a5cb13c8495737f30
Francesco Gazzetta <fgaz@fgaz.me>
2022-06-29 16:15:48 +0200

    Revert "Add defaultPackage"
    This reverts commit 767223a7e33e14e119bee3d8c3884ea39cc0587e.

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
   outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: rec {
     packages.nix-bubblewrap = import ./. { pkgs = nixpkgs.legacyPackages."${system}"; };
     packages.default = packages.nix-bubblewrap;
-    defaultPackage = packages.nix-bubblewrap;
     lib = import ./lib.nix {
       pkgs = nixpkgs.legacyPackages."${system}";
       nix-bubblewrap = packages.nix-bubblewrap;
